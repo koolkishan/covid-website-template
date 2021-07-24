6e46a39e5b8d5a4e0f76db39ee4b660ed7d40909
koolkishan <kishansheth21@gmail.com>
2021-07-24 22:15:15 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const preloader = document.querySelector(".preloader");
+
+const bodyLoaded = () => {
+  setTimeout(() => {
+    preloader.style.display = "none";
+  }, 1000);
+};
+
+const scrollToTop = document.querySelector(".scrollToTop");
+
+window.addEventListener("scroll", () => {
+  window.pageYOffset > 400
+    ? (scrollToTop.style.display = "block")
+    : (scrollToTop.style.display = "none");
+});
+
+scrollToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0 });
+});
+
+let navBarState = false;
+
+const navbarToggle = document.querySelector(".responsive-toggle");
+
+navbarToggle.addEventListener("click", () => {
+  const navlinks = document.querySelector(".links");
+  if (navBarState) {
+    navlinks.style.top = "-40rem";
+    navBarState = false;
+  } else {
+    navlinks.style.top = "-4rem";
+    navBarState = true;
+  }
+});
